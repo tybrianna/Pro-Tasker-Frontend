41810d54682f94e57e5f0e77993d33a6067bd326
tybrianna <tybriannahall@gmail.com>
2026-06-05 06:47:13 -0400

    made correction to api file

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -4,15 +4,14 @@ const API = axios.create({
   baseURL: "http://localhost:5000/api",
 });
 
-API.interceptors.request.use((req) => {
+API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
   if (token) {
-    req.headers.Authorization =
-      `Bearer ${token}`;
+    config.headers.Authorization = `Bearer ${token}`;
   }
 
-  return req;
+  return config;
 });
 
 export default API;
