165f41caea779b657a4832b4ed70293855e0c55b
tybrianna <tybriannahall@gmail.com>
2026-06-05 06:50:16 -0400

    made correction to main.tsx file. There is something with a red underline but cannot determine the issue. Will double check to make sure all packages have been added.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,13 +4,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 import {
-  AuthProvider
+  AuthProvider,
 } from "./context/AuthContext";
 
 ReactDOM.createRoot(
-  document.getElementById("root")
+  document.getElementById("root")!
 ).render(
-  <AuthProvider>
-    <App />
-  </AuthProvider>
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
 );
